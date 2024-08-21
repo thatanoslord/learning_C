babad3229f3d870eda49abdb6510764d5f89cad2
Moussa Sabbar <sabbarmoussa@gmail.com>
2024-08-21 15:51:53 +0100

    new repo

---
+++ learning_c/printf.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/*
+ * this how we write comments in c
+ */
+int main(){
+	
+	int age = 26;
+	char moussa[6];
+	moussa[0] = 'M';
+	moussa[1] = 'o';
+	moussa[2] = 'u';
+	moussa[3] = 's';
+	moussa[4] = 's';
+	moussa[5] = 'a';
+	
+	printf("My name is %s, and I'am %d\n ", moussa, age);
+	printf("===================================\n");
+	printf("%d is an intiger\n",10);
+	return(0);
+}
