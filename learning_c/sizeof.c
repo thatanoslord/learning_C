e6f85031f944929f2472667b46825c5e59132e3c
Moussa Sabbar <sabbarmoussa@gmail.com>
2024-08-21 16:43:54 +0100

    learning sizeof operator

---
+++ learning_c/sizeof.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+	printf("int size: %ld\n", sizeof(int));
+	printf("float size: %ld\n", sizeof(float));
+	printf("double size: %ld\n", sizeof(double));
+	printf("char size: %ld\n", sizeof(char));
+	printf("long size: %ld\n", sizeof(long));
+	return 0;
+}
