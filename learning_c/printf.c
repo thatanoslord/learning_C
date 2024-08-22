253a37e74287fead55c2af13c825d984af7e1e3a
Moussa Sabbar <sabbarmoussa@gmail.com>
2024-08-22 01:00:55 +0100

    new thing

--- learning_c/printf.c
+++ learning_c/printf.c
@@ -2,19 +2,20 @@
 /*
  * this how we write comments in c
  */
-int main(){
-	
+
+int main(void)
+{
 	int age = 26;
 	char moussa[6];
+
 	moussa[0] = 'M';
 	moussa[1] = 'o';
 	moussa[2] = 'u';
 	moussa[3] = 's';
 	moussa[4] = 's';
 	moussa[5] = 'a';
-	
 	printf("My name is %s, and I'am %d\n ", moussa, age);
 	printf("===================================\n");
-	printf("%d is an intiger\n",10);
-	return(0);
+	printf("%d is an intiger\n", 10);
+	return (0);
 }
